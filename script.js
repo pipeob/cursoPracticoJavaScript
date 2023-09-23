72c13f0335c25115f2357415e9e67bceee96af94
pipeob <javierfelipeortiz17@gmail.com>
2023-09-23 01:49:36 -0500

    Agregue html, css, js para programa calculadora

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hola mundo');
+const input = document.querySelector('#input');
+const display = document.querySelector('.display');
+const btnIgual = document.querySelector('#igual');
+
+btnIgual.addEventListener('click', operacionSuma);
+
+function operacionSuma(){
+    const suma = input.value
+}
