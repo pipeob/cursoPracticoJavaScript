317fec6672f777424f6f5a060f48be0c1b83bff4
pipeob <javierfelipeortiz17@gmail.com>
2023-09-23 22:53:15 -0500

    Calculadora con funcion suma y resta incompleta

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const input = document.querySelector('#input');
 const display = document.querySelector('.display');
 const btnIgual = document.querySelector('#igual');
+const btnSuma = document.querySelector('#suma');
+const btnResta = document.querySelector('#resta');
+const opcion = document.querySelector('.op');
 
-btnIgual.addEventListener('click', operacionSuma);
+let resultado;
 
-function operacionSuma(){
-    const suma = input.value
+btnSuma.addEventListener('click', opSuma);
+
+function opSuma(){
+    resultado = Number(input.value) + 5;
 }
+
+btnIgual.addEventListener('click', opIgual);
+
+function opIgual(){
+    input.value = resultado;
+}
+
+btnResta.addEventListener('click', opResta);
+
+function opResta(){
+    resultado = Number(input.value) - 5;
+}
+
+// opcion.addEventListener('click', funOpcion);
+
+// function funOpcion(){
+//     input.value = ;
+// }
